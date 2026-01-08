24891e713fb99f26e0990a07c6e1923803fec02d
rntrwsn3828-coder <rntrwsn3828@gmail.com>
2026-01-08 11:54:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,11 @@
-const CACHE_NAME = 'bowlliard-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', e => {
+self.addEventListener('install', event => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', event => {
+  // 何もしない
 });
